24fc3205ad13f038c62162678e8502cd2fccd917
nootdy <preeyanoot2013@gmail.com>
2022-03-18 14:18:35 +0700

    Add project files.

---
+++ DotnetJwtWebApi/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetJwtWebApi.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } 
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ DotnetJwtWebApi/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetJwtWebApi
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
